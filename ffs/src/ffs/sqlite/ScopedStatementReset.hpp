239c4baa0c267cd6d2158ee91f019bc7916dba9e
Zachary Sims <zac.sims@gmail.com>
2016-05-18 19:37:34 +1000

    Adds a ScopedStatementReset that resets a statement on destruction

---
+++ ffs/src/ffs/sqlite/ScopedStatementReset.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <sqlite3.h>
+
+namespace af {
+namespace ffs {
+namespace sqlite {
+
+/**
+ * Resets a statement using sqlite3_reset, meaning the statement can be re-used.
+ * Note that bindings are also reset.
+ */
+class ScopedStatementReset
+{
+public:
+	ScopedStatementReset(const ScopedStatementReset& that) = delete;
+	ScopedStatementReset& operator=(const ScopedStatementReset&) = delete;
+
+	explicit ScopedStatementReset(sqlite3_stmt* statement)
+		: _statement(statement)
+	{
+	}
+
+	~ScopedStatementReset()
+	{
+		sqlite3_reset(_statement);
+		sqlite3_clear_bindings(_statement);
+	}
+private:
+	sqlite3_stmt* _statement;
+};
+
+}
+}
+}
