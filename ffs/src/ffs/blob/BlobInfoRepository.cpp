8d1543ad1af3ccf80e092040f15ef821fbfc749f
Zachary Sims <zac.sims@gmail.com>
2016-05-19 21:32:47 +1000

    Uses prepared statements in the blob info repository

--- ffs/src/ffs/blob/BlobInfoRepository.cpp
+++ ffs/src/ffs/blob/BlobInfoRepository.cpp
@@ -4,8 +4,7 @@
 #include "ffs/blob/BlobInfo.hpp"
 #include "ffs/blob/exceptions.hpp"
 #include "ffs/exceptions.hpp"
-#include "ffs/sqlitepp/handles.hpp"
-#include "ffs/sqlitepp/ScopedTransaction.hpp"
+#include "ffs/sqlitepp/sqlitepp.hpp"
 
 #include <boost/format.hpp>
 #include <sqlite3.h>
@@ -17,39 +16,38 @@ namespace af {
 namespace ffs {
 namespace blob {
 
-BlobInfoRepository::BlobInfoRepository(const std::string& utf8DbPath)
-	: _db(0)
+namespace {
+enum GetAllBlobsColumnIndex
 {
-	const auto result = sqlite3_open_v2(utf8DbPath.c_str(), &_db, SQLITE_OPEN_READWRITE, 0);
+	GetAllBlobs_ColumnIndex_Address = 0,
+	GetAllBlobs_ColumnIndex_SizeBytes
+};
+}
+
+BlobInfoRepository::BlobInfoRepository(const std::string& utf8DbPath)
+{
+	const auto result = sqlite3_open_v2(utf8DbPath.c_str(), _db, SQLITE_OPEN_READWRITE, 0);
 	if (result != SQLITE_OK)
 	{
 		throw OpenDatabaseFailedException((boost::format("Cannot open database at %1%. SQLite returned %2%") % utf8DbPath % result).str());
 	}
-}
 
-BlobInfoRepository::~BlobInfoRepository()
-{
-	sqlite3_close_v2(_db);
+	sqlitepp::prepare_or_throw(_db, "INSERT INTO Blob (Address, SizeBytes) VALUES (:Address, :SizeBytes)", _insertBlobStatement);
+	sqlitepp::prepare_or_throw(_db, "SELECT Address, SizeBytes FROM Blob", _getAllBlobsStatement);
 }
 
 std::vector<BlobInfoModelPtr> BlobInfoRepository::GetAllBlobs() const
 {
 	std::vector<BlobInfoModelPtr> result;
-
 	sqlitepp::ScopedTransaction transaction(_db);
-	sqlitepp::ScopedStatement statement;
-	const auto prepareResult = sqlite3_prepare_v2(_db, "SELECT Address, SizeBytes FROM Blob", -1, statement, 0);
-	if (prepareResult != SQLITE_OK)
-	{
-		throw GetBlobsFailedException((boost::format("Failed to prepare get all blobs statement. SQLite error %1%") % prepareResult).str());
-	}
+	sqlitepp::ScopedStatementReset reset(_getAllBlobsStatement);
 
 	auto stepResult = 0;
-	while ((stepResult = sqlite3_step(statement)) == SQLITE_ROW)
+	while ((stepResult = sqlite3_step(_getAllBlobsStatement)) == SQLITE_ROW)
 	{
 		binary_address address;
-		const auto addressBytesCount = sqlite3_column_bytes(statement, 0);
-		const auto addressBytes = sqlite3_column_blob(statement, 0);
+		const auto addressBytesCount = sqlite3_column_bytes(_getAllBlobsStatement, GetAllBlobs_ColumnIndex_Address);
+		const auto addressBytes = sqlite3_column_blob(_getAllBlobsStatement, GetAllBlobs_ColumnIndex_Address);
 
 		if (addressBytesCount > static_cast<int>(address.max_size()))
 		{
@@ -57,7 +55,7 @@ std::vector<BlobInfoModelPtr> BlobInfoRepository::GetAllBlobs() const
 		}
 
 		std::copy_n(static_cast<const uint8_t*>(addressBytes), addressBytesCount, address.begin());
-		const auto sizeBytes = static_cast<uint64_t>(sqlite3_column_int64(statement, 1));
+		const auto sizeBytes = static_cast<uint64_t>(sqlite3_column_int64(_getAllBlobsStatement, GetAllBlobs_ColumnIndex_SizeBytes));
 		result.push_back(std::make_shared<BlobInfo>(BlobAddress(address), sizeBytes));
 	}
 
@@ -71,25 +69,20 @@ void BlobInfoRepository::AddBlob(const BlobInfo& info)
 	// binary address, note this has to be kept in scope until SQLite has finished as we've opted not to make a copy
 	const auto binaryAddress = info.GetAddress().ToBinary();
 
-	sqlitepp::ScopedTransaction transaction(_db);
-	sqlitepp::ScopedStatement statement;
-	const auto prepareResult = sqlite3_prepare_v2(_db, "INSERT INTO Blob (Address, SizeBytes) VALUES (?, ?)", -1, statement, 0);
-	if (prepareResult != SQLITE_OK)
-	{
-		// Note that statement doesn't need to be free'd if prepare fails
-		throw AddBlobFailedException((boost::format("Failed to prepare blob insert statement for %1%. SQLite error %2%") % info.GetAddress().ToString() % prepareResult).str());
-	}
+	const auto addressIndex = sqlite3_bind_parameter_index(_insertBlobStatement, ":Address");
+	const auto sizeBytesIndex = sqlite3_bind_parameter_index(_insertBlobStatement, ":SizeBytes");
 
+	sqlitepp::ScopedTransaction transaction(_db);
+	sqlitepp::ScopedStatementReset reset(_insertBlobStatement);
 	{
-		const auto bindResult = sqlite3_bind_blob(statement, 1, &binaryAddress[0], static_cast<int>(binaryAddress.size()), 0);
+		const auto bindResult = sqlite3_bind_blob(_insertBlobStatement, addressIndex, &binaryAddress[0], static_cast<int>(binaryAddress.size()), 0);
 		if (bindResult != SQLITE_OK)
 		{
 			throw AddBlobFailedException((boost::format("Failed to bind blob parameter address %1%. SQLite error %2%") % info.GetAddress().ToString() % bindResult).str());
 		}
 	}
-
 	{
-		const auto bindResult = sqlite3_bind_int64(statement, 2, static_cast<int64_t>(info.GetSizeBytes()));
+		const auto bindResult = sqlite3_bind_int64(_insertBlobStatement, sizeBytesIndex, static_cast<int64_t>(info.GetSizeBytes()));
 		if (bindResult != SQLITE_OK)
 		{
 			throw AddBlobFailedException((boost::format("Failed to bind blob parameter size %1%. SQLite error %2%") % info.GetAddress().ToString() % bindResult).str());
@@ -97,7 +90,7 @@ void BlobInfoRepository::AddBlob(const BlobInfo& info)
 	}
 
 	// execute
-	const auto stepResult = sqlite3_step(statement);
+	const auto stepResult = sqlite3_step(_insertBlobStatement);
 	if (stepResult != SQLITE_DONE)
 	{
 		if (stepResult == SQLITE_CONSTRAINT)
