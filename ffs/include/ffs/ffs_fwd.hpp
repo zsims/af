3e25f215bea4ef149c56b08d054be82f79e9fd94
Zachary Sims <zac.sims@gmail.com>
2016-06-21 17:21:12 +1000

    Adds a forward declare header for common FFS classes

---
+++ ffs/include/ffs/ffs_fwd.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace af {
+namespace ffs {
+
+class Address;
+class BlobAddress;
+class Forest;
+class ObjectAddress;
+class UnitOfWork;
+
+namespace blob {
+
+class BlobInfo;
+class BlobStore;
+
+}
+
+namespace object {
+
+class ObjectInfo;
+
+}
+
+}
+}
