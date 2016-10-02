c11ea03a94bab6ab6b81d94c0a4931619ff23cf8
Zachary Sims <zac.sims@gmail.com>
2016-10-02 20:29:42 +1100

    Uses the TestBase in the Forest Integration Test

--- bslib/test/src/ForestIntegrationTest.cpp
+++ bslib/test/src/ForestIntegrationTest.cpp
@@ -6,6 +6,7 @@
 #include "bslib/file/exceptions.hpp"
 #include "bslib/file/FileAdder.hpp"
 #include "bslib/file/fs/operations.hpp"
+#include "TestBase.hpp"
 
 #include <boost/filesystem.hpp>
 #include <gtest/gtest.h>
@@ -17,56 +18,32 @@ namespace af {
 namespace bslib {
 namespace test {
 
-class ForestIntegrationTest : public testing::Test
+class ForestIntegrationTest : public TestBase
 {
 protected:
-	ForestIntegrationTest()
-		: _directoryBlobStorePath(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path())
-		, _forestDbPath(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path("%%%%-%%%%-%%%%-%%%%.fdb"))
-	{
-		boost::filesystem::create_directories(_directoryBlobStorePath);
-		auto blobStore = std::make_unique<blob::DirectoryBlobStore>(_directoryBlobStorePath.string());
-		_forest.reset(new Forest(_forestDbPath.string(), std::move(blobStore)));
-	}
-
-	~ForestIntegrationTest()
-	{
-		_forest.reset();
-
-		boost::system::error_code rec;
-		boost::filesystem::remove_all(_directoryBlobStorePath, rec);
-
-		boost::system::error_code ec;
-		boost::filesystem::remove(_forestDbPath, ec);
-	}
-
 	void CreateFile(const boost::filesystem::path& path)
 	{
 		std::ofstream f(path.string(), std::ofstream::out | std::ofstream::app);
 	}
-
-	const boost::filesystem::path _directoryBlobStorePath;
-	const boost::filesystem::path _forestDbPath;
-	std::unique_ptr<Forest> _forest;
 };
 
 TEST_F(ForestIntegrationTest, CreateSuccess)
 {
 	// Arrange
 	// Act
-	_forest->Create();
+	_testForest.CreateWithNullStore();
 	// Assert
-	EXPECT_TRUE(boost::filesystem::exists(_forestDbPath.string()));
+	EXPECT_TRUE(boost::filesystem::exists(_testForest.GetForestDbPath()));
 }
 
 TEST_F(ForestIntegrationTest, CreateThrowsIfExists)
 {
 	// Arrange
-	CreateFile(_forestDbPath);
+	CreateFile(_testForest.GetForestDbPath());
 
 	// Act
 	// Assert
-	EXPECT_THROW(_forest->Create(), DatabaseAlreadyExistsException);
+	EXPECT_THROW(_testForest.Create(), DatabaseAlreadyExistsException);
 }
 
 TEST_F(ForestIntegrationTest, OpenThrowsIfNotExists)
@@ -74,17 +51,18 @@ TEST_F(ForestIntegrationTest, OpenThrowsIfNotExists)
 	// Arrange
 	// Act
 	// Assert
-	EXPECT_THROW(_forest->Open(), DatabaseNotFoundException);
+	EXPECT_THROW(_testForest.Open(), DatabaseNotFoundException);
 }
 
 TEST_F(ForestIntegrationTest, UnitOfWorkCommit)
 {
 	// Arrange
-	_forest->Create();
+	_testForest.CreateWithNullStore();
+	auto& forest = _testForest.GetForest();
 
 	const auto targetPath = file::fs::GenerateUniqueTempPath().EnsureTrailingSlash();
 	{
-		auto uow = _forest->CreateUnitOfWork();
+		auto uow = forest.CreateUnitOfWork();
 		auto adder = uow->CreateFileAdder();
 		file::fs::CreateDirectories(targetPath);
 		adder->Add(targetPath.ToString());
@@ -94,7 +72,7 @@ TEST_F(ForestIntegrationTest, UnitOfWorkCommit)
 
 	// Assert
 	{
-		auto uow = _forest->CreateUnitOfWork();
+		auto uow = forest.CreateUnitOfWork();
 		auto finder = uow->CreateFileFinder();
 		EXPECT_TRUE(finder->FindLastChangedEventByPath(targetPath));
 	}
@@ -103,24 +81,26 @@ TEST_F(ForestIntegrationTest, UnitOfWorkCommit)
 TEST_F(ForestIntegrationTest, CreateUnitOfWorkTwiceFails)
 {
 	// Arrange
-	_forest->Create();
+	_testForest.Create();
+	auto& forest = _testForest.GetForest();
 
 	// Act
 	// Assert
 	// No support for multiple connections yet, so this isn't possible
-	auto uow1 = _forest->CreateUnitOfWork();
-	EXPECT_THROW(_forest->CreateUnitOfWork(), std::runtime_error);
+	auto uow1 = forest.CreateUnitOfWork();
+	EXPECT_THROW(forest.CreateUnitOfWork(), std::runtime_error);
 }
 
 TEST_F(ForestIntegrationTest, UnitOfWorkImplicitRollback)
 {
 	// Arrange
-	_forest->Create();
+	_testForest.Create();
+	auto& forest = _testForest.GetForest();
 
 	// Act
 	const auto targetPath = file::fs::GenerateUniqueTempPath().EnsureTrailingSlash();
 	{
-		auto uow = _forest->CreateUnitOfWork();
+		auto uow = forest.CreateUnitOfWork();
 		auto adder = uow->CreateFileAdder();
 		file::fs::CreateDirectories(targetPath);
 		adder->Add(targetPath.ToString());
@@ -128,7 +108,7 @@ TEST_F(ForestIntegrationTest, UnitOfWorkImplicitRollback)
 
 	// Assert
 	{
-		auto uow = _forest->CreateUnitOfWork();
+		auto uow = forest.CreateUnitOfWork();
 		auto finder = uow->CreateFileFinder();
 		EXPECT_FALSE(finder->FindLastChangedEventByPath(targetPath));
 	}
@@ -137,9 +117,9 @@ TEST_F(ForestIntegrationTest, UnitOfWorkImplicitRollback)
 TEST_F(ForestIntegrationTest, OpenOrCreateExisting)
 {
 	// Arrange
-	_forest->Create();
-	_forest.reset();
-	Forest secondForest(_forestDbPath, std::make_unique<blob::NullBlobStore>());
+	_testForest.Create();
+	_testForest.Close();
+	Forest secondForest(_testForest.GetForestDbPath(), std::make_unique<blob::NullBlobStore>());
 
 	// Act
 	// Assert
@@ -151,7 +131,7 @@ TEST_F(ForestIntegrationTest, OpenOrCreateNew)
 	// Arrange
 	// Act
 	// Assert
-	ASSERT_NO_THROW(_forest->OpenOrCreate());
+	ASSERT_NO_THROW(_testForest.OpenOrCreate());
 }
 
 }
