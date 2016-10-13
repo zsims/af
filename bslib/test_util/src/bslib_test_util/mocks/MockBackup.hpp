847cde8573f25b1793d6ca860a78b5bfedc95dba
Zachary Sims <zac.sims@gmail.com>
2016-10-13 22:46:44 +1100

    Adds simple job executor for executing backup jobs in order

--- bslib/test_util/src/bslib_test_util/mocks/MockBackup.hpp
+++ bslib/test_util/src/bslib_test_util/mocks/MockBackup.hpp
@@ -11,9 +11,21 @@ namespace mocks {
 class MockBackup : public bslib::Backup
 {
 public:
+	MockBackup() 
+		: bslib::Backup("Not Real", "Testing")
+	{
+
+	}
 	virtual ~MockBackup() { }
+
+	// Support mocking unique_ptr parameters per https://github.com/google/googletest/blob/master/googlemock/docs/CookBook.md#mocking-methods-that-use-move-only-types
+	virtual void AddBlobStore(std::unique_ptr<bslib::blob::BlobStore> blobStore) override
+	{
+		return AddBlobStoreProxy(blobStore.get());
+	}
+
 	MOCK_METHOD0(CreateUnitOfWork, std::unique_ptr<bslib::UnitOfWork>());
-	MOCK_METHOD1(AddBlobStore, void(std::unique_ptr<bslib::blob::BlobStore> blobStore));
+	MOCK_METHOD1(AddBlobStoreProxy, void(bslib::blob::BlobStore* blobStore));
 	MOCK_METHOD0(Open, void());
 	MOCK_METHOD0(Create, void());
 	MOCK_METHOD0(OpenOrCreate, void());
