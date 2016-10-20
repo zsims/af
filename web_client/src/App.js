2ffb54272507c6eacbdbadc9e201a299ef9efa75
Philip Peitsch <philip.peitsch@gmail.com>
2016-10-20 12:29:07 +1100

    Create simple web_client skeleton using React + Material-UI

---
+++ web_client/src/App.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
+import Divider from 'material-ui/Divider';
+
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import ActionRestore from 'material-ui/svg-icons/action/restore';
+import ActionSettings from 'material-ui/svg-icons/action/settings';
+import AvLibraryBooks from 'material-ui/svg-icons/av/library-books';
+import AlertWarning from 'material-ui/svg-icons/alert/warning';
+
+const muiTheme = getMuiTheme({
+  appBar: {
+    height: 50,
+  },
+});
+
+const style = {
+  menu: {
+    float: 'left'
+  }
+};
+
+
+export default class App extends Component {
+  static contextTypes = {
+    router: React.PropTypes.object
+  };
+
+  state = {
+    value: "/"
+  };
+
+  navigateTo = (event, value) => {
+    this.setState({value: value});
+    this.context.router.push(value);
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="App">
+          <AppBar title="af backup" showMenuIconButton={false}/>
+          <Menu style={style.menu} onChange={this.navigateTo} value={this.state.value}>
+            <MenuItem value="/" leftIcon={<ActionInfo/>}>Summary</MenuItem>
+            <MenuItem value="/restore" leftIcon={<ActionRestore/>}>Restore files</MenuItem>
+            <Divider />
+            <MenuItem value="/settings" leftIcon={<ActionSettings/>}>Settings</MenuItem>
+            <MenuItem value="/systemlogs" leftIcon={<AvLibraryBooks/>}>System Logs</MenuItem>
+          </Menu>
+          {this.props.children}
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
---
+++ web_client/src/modules/Summary.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Summary extends Component {
+  render() {
+    return (
+      <div className="Summary">
+        You probably expect to see some info about the current service status here :D
+      </div>
+    );
+  }
+}
