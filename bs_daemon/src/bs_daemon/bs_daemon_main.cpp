0546293bcec1736744eef09926a8c3cb1cc4fb60
Zachary Sims <zac.sims@gmail.com>
2016-10-21 09:33:27 +1100

    Opens the default af database when the daemon is started

--- bs_daemon/src/bs_daemon/bs_daemon_main.cpp
+++ bs_daemon/src/bs_daemon/bs_daemon_main.cpp
@@ -1,19 +1,55 @@
+#include "bslib/Backup.hpp"
 #include "bs_daemon_lib/log.hpp"
 #include "bs_daemon_lib/HttpServer.hpp"
 
+#include <boost/filesystem.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 
 #include <iostream>
 #include <conio.h>
 
-int main(int argc, char* argv[])
+namespace {
+
+int Run()
 {
-	boost::log::add_common_attributes();
+	const auto defaultDbPath = af::bslib::GetDefaultBackupDatabasePath();
+	if (defaultDbPath.empty())
+	{
+		BS_DAEMON_LOG_FATAL << "Failed to determine the location of the backup database";
+		return -1;
+	}
+	BS_DAEMON_LOG_INFO << "Using the backup database from " << defaultDbPath << std::endl;
+	boost::filesystem::create_directories(defaultDbPath.parent_path());
+
+	af::bslib::Backup backup(defaultDbPath, "CLI");
+	backup.OpenOrCreate();
+
 	af::bs_daemon::HttpServer server(8080);
 
 	std::cout << "Press any key to exit" << std::endl;
 	_getch();
 	std::cout << "Shutting down..." << std::endl;
-
 	return 0;
+}
+
+}
+
+int main(int argc, char* argv[])
+{
+	boost::log::add_common_attributes();
+
+	try
+	{
+		return Run();
+	}
+	catch (const std::exception& e)
+	{
+		BS_DAEMON_LOG_FATAL << "Unhandled exception: " << e.what();
+		return 1;
+	}
+	catch (...)
+	{
+		BS_DAEMON_LOG_FATAL << "Unhandled exception";
+		return -1;
+	}
 }
