8930b9011cb8e3468eaf54982ecee78cf1c5a95b
Zachary Sims <zac.sims@gmail.com>
2016-10-21 21:20:40 +1100

    Adds basic ping-pong integration tests for the daemon HTTP interface

--- bs_daemon/src/bs_daemon_lib/HttpServer.cpp
+++ bs_daemon/src/bs_daemon_lib/HttpServer.cpp
@@ -20,21 +20,39 @@ HttpServer::HttpServer(int port, JobExecutor& jobExecutor)
 	, _simpleServer(port, /* number of threads = */ 1)
 	, _serverThread(&HttpServer::Run, this)
 {
+	_simpleServer.config.address = "127.0.0.1";
+
 	_simpleServer.default_resource["GET"] = [&](std::shared_ptr<SimpleServer::Response> response, std::shared_ptr<SimpleServer::Request> request) {
-		const std::string index("<html><head><title>AF Backup Service</title></head><body>Hi there</body></html>");
-		*response << "HTTP/1.1 200 OK\r\nContent-Length:" << index.size() << "\r\n\r\n" << index;
+		*response << "HTTP/1.1 404 Not Found\r\n\r\n";
 	};
 
 	_simpleServer.resource["^/file$"]["POST"] = [&](std::shared_ptr<SimpleServer::Response> response, std::shared_ptr<SimpleServer::Request> request) {
 		try {
 			boost::property_tree::ptree pt;
 			boost::property_tree::read_json(request->content, pt);
+
 			auto path = pt.get<std::string>("path");
 
 			// kick off the job
 			_jobExecutor.Queue(std::make_unique<FileBackupJob>(path));
 
 			*response << "HTTP/1.1 204 No Content\r\n\r\n";
+		}
+		catch (std::exception& e) {
+			*response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
+		}
+	};
+
+	// Test API that takes JSON and deserializes it, then sends it back as JSON
+	_simpleServer.resource["^/ping$"]["POST"] = [&](std::shared_ptr<SimpleServer::Response> response, std::shared_ptr<SimpleServer::Request> request) {
+		try {
+			// Note this should support UTF-8, according to the property tree docs: https://svn.boost.org/trac/boost/ticket/8883
+			boost::property_tree::ptree pt;
+			boost::property_tree::read_json(request->content, pt);
+			std::stringstream ss;
+			boost::property_tree::write_json(ss, pt);
+			const auto content = ss.str();
+			*response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\nContent-Type: application/json; charset=utf-8\r\n\r\n" << content;
 		}
 		catch (std::exception& e) {
 			*response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
@@ -49,7 +67,7 @@ HttpServer::~HttpServer()
 
 void HttpServer::Run()
 {
-	BS_DAEMON_LOG_INFO << "Starting HTTP server";
+	BS_DAEMON_LOG_INFO << "Starting HTTP server on " << _simpleServer.config.address << ":" << _simpleServer.config.port;
 	// blocks until the server stops
 	_simpleServer.start();
 }
---
+++ bs_daemon/test/src/HttpServerIntegrationTest.cpp
@@ -0,0 +1,98 @@
+﻿#include "bs_daemon_lib/HttpServer.hpp"
+
+#include "bslib_test_util/TestBase.hpp"
+#include "bslib_test_util/mocks/MockBackup.hpp"
+#include "bslib_test_util/mocks/MockUnitOfWork.hpp"
+
+// Work around https://svn.boost.org/trac/boost/ticket/11599
+#pragma warning( push )
+#pragma warning( disable : 4715)
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#pragma warning( pop )
+
+// Avoid size_t <-> int64 warnings on Windows x32
+#pragma warning( push )
+#pragma warning( disable : 4244 )
+#include <client_http.hpp>
+#pragma warning( pop )
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <random>
+
+typedef SimpleWeb::Client<SimpleWeb::HTTP> HttpClient;
+
+namespace {
+std::random_device randomDevice;
+std::mt19937 random(randomDevice());
+std::uniform_int_distribution<int> randomUniformDistribution(45000, 46000);
+
+int FindFreePort()
+{
+	// It's not possible to call bind(address, 0) with the simple http server as it doesn't expose the port it binds to
+	return randomUniformDistribution(random);
+}
+
+}
+
+namespace af {
+namespace bs_daemon {
+namespace test {
+
+class HttpServerIntegrationTest : public ::testing::Test
+{
+protected:
+	HttpServerIntegrationTest()
+		: _testPort(FindFreePort())
+		, _testAddress("127.0.0.1:" + std::to_string(_testPort))
+		, _jobExecutor(_mockBackup)
+		, _httpServer(_testPort, _jobExecutor)
+	{
+		ON_CALL(_mockBackup, CreateUnitOfWork())
+			.WillByDefault(::testing::Invoke([]() {
+			return std::make_unique<bslib_test_util::mocks::MockUnitOfWork>();
+		}));
+	}
+	bslib_test_util::mocks::MockBackup _mockBackup;
+	const int _testPort;
+	const std::string _testAddress;
+	JobExecutor _jobExecutor;
+	HttpServer _httpServer;
+};
+
+TEST_F(HttpServerIntegrationTest, UnregisteredEndpoints_Return404)
+{
+	// Arrange
+	HttpClient client(_testAddress);
+
+	// Act
+	auto response = client.request("GET", "/match/123");
+
+	// Assert
+	EXPECT_EQ(response->status_code, "404 Not Found");
+}
+
+TEST_F(HttpServerIntegrationTest, Ping_Success)
+{
+	// Arrange
+	HttpClient client(_testAddress);
+	const auto rawContent = u8R"({"fancy":"Сука Блять"})";
+	const auto expectedFancy = u8"Сука Блять";
+
+	// Act
+	auto response = client.request("POST", "/ping", rawContent);
+
+	// Assert
+	ASSERT_EQ(response->status_code, "200 OK");
+	boost::property_tree::ptree pt;
+	boost::property_tree::read_json(response->content, pt);
+	auto value = pt.get_optional<std::string>("fancy");
+	ASSERT_TRUE(value);
+	EXPECT_EQ(expectedFancy, value.get());
+}
+
+}
+}
+}
